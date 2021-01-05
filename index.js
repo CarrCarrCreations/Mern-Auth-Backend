671c1bf5d1bd07d1677577d7413aa9759817cd55
Liam Carr <liam@liamcarr.com>
2021-01-04 19:37:13 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+// setup express
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`The server has started on port: ${PORT}`);
+});
+
+// Setup mongoose
+mongoose.connect(
+  process.env.MONGODB_CONNECTION_STRING,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("MongoDB connection established.");
+  }
+);
+
+// Setup routes
+app.use("/users", require("./routes/userRouter"));
