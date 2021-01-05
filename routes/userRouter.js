e6abdc21a8fadd95d9271351f724effbbeb3ebaa
Liam Carr <liam@liamcarr.com>
2021-01-05 12:59:56 -0500

    CHange router method from get to post
    get request would not attach the x-auth header, only a post method would

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,7 +5,7 @@ const User = require("../models/userModel");
 // Since we are using auth, we can get the userId from req.user.id
 // You get all information from the DB based on the currently
 // logged in user. It is all saved in req.user
-router.get("/", auth, async (req, res) => {
+router.post("/", auth, async (req, res) => {
   const user = await User.findById(req.user.id);
   res.json({
     id: user._id,
