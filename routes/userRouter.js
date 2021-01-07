461d80329e1b54f6cab05e2b71355f23298b1637
Liam Carr <liam@liamcarr.com>
2021-01-07 12:33:20 -0500

    Refactor userRouter to use userService

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const auth = require("../middleware/auth");
+import { findUserById } from "../service/UserService";
 
 // Auth middleware saves logged in users data to req.user
 router.post("/", auth, async (req, res) => {
   try {
-    const user = await User.findById(req.user.id);
+    const user = await findUserById(req.user.id);
     return res.status(200).json(user);
   } catch (err) {
     return res.status(500).json();
