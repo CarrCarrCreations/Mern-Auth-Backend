f2ad76d8e6be00845605e8fe417651f72380eba5
Liam Carr <liam@liamcarr.com>
2021-01-08 06:35:26 -0500

    Fix incorrect import statements and duplicate function name

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const auth = require("../middleware/auth");
-import { findUserById } from "../service/UserService";
+const findUser = require("../service/UserService");
 
 // Auth middleware saves logged in users data to req.user
 router.post("/", auth, async (req, res) => {
   try {
-    const user = await findUserById(req.user.id);
+    const user = await findUser(req.user.id);
     return res.status(200).json(user);
   } catch (err) {
     return res.status(500).json();
--- service/UserService.js
+++ service/UserService.js
@@ -1,14 +1,14 @@
-import {findUserById} from "../repository/UserRepository"
+const findUserById = require("../repository/UserRepository");
 
-const findUserById = (id) => {
-    const user = await findById(id);
-    if(!user) throw "No user found for given ID"
-    return {
-        id: user._id,
-        displayName: user.displayName,
-      }
+const findUser = async (id) => {
+  const user = await findById(id);
+  if (!user) throw "No user found for given ID";
+  return {
+    id: user._id,
+    displayName: user.displayName,
+  };
 };
 
 module.exports = {
-  findUserById,
+  findUser,
 };
